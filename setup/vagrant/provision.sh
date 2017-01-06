d0de1bce25d854344e25a621bb405d3ce1949012
sseifert <sseifert@pro-vision.de>
2017-01-06 17:16:00 +0100

    update provisioning

--- setup/vagrant/provision.sh
+++ setup/vagrant/provision.sh
@@ -11,7 +11,10 @@ sudo apt-get install -y redis-server
 # 3. PostgreSQL
 sudo apt-get -y install postgresql-9.3 postgresql-server-dev-9.3
 # 4. Node.js
-sudo apt-get install -y nodejs node npm
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs build-essential
+# git
+sudo apt-get install -y git
 # bower
 sudo npm install -g bower
 
@@ -24,10 +27,15 @@ sudo pip install -r /opt/redash/current/requirements_dev.txt
 sudo pip install -r /opt/redash/current/requirements.txt
 sudo pip install pymongo==3.2.1
 
-#update database
-#bin/run ./manage.py database drop_tables
-#bin/run ./manage.py database create_tables
-#bin/run ./manage.py users create --admin --password admin "Admin" "admin"
-#
-##Purge Redis cache
-#redis-cli -n 1 FLUSHALL
+#create database
+sudo -u postgres createuser vagrant --no-superuser --no-createdb --no-createrole
+sudo -u postgres createdb redash --owner=vagrant
+bin/run ./manage.py database create_tables
+bin/run ./manage.py users create --admin --password admin "Admin" "admin"
+
+#Purge Redis cache
+redis-cli -n 1 FLUSHALL
+
+#prepare client
+#cd /opt/redash/current/client
+#sudo npm install
